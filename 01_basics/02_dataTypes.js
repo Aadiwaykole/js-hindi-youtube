c2e3af5f07879c41969be45c643d1744863037bc
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-24 19:56:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict "; //treat all js code as newer version
+
+// alert(3 +3) //WE ARE using nodejs, not browser
+
+console.log(3) //code readability should be high
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
