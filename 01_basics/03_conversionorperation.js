157c897b2adf7def4e7292d2c48856daedf1aa42
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-24 20:36:51 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionorperation.js
@@ -0,0 +1,33 @@
+let score = 34 //number
+
+let scope = "33" //string
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+console.log(typeof scope);
+
+// score can directly change into 
+let valueInNumber = Number(scope)
+console.log(typeof valueInNumber);
+
+
+let scopee = null
+let valueNumber = Number(scope)
+console.log(typeof valueNumber);
+console.log(valueNumber)
+
+
+// "33" => 33
+// "33abc"=> NaN
+// true =>1; false=>0
+
+let isLoggedIn = null
+
+let boolearnIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolearnIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" =>false
+// "adi" => true 
