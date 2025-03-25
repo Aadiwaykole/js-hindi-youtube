62cd4fc8c0129f4569e2bdf1846fd4f80ca82297
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-25 17:58:15 +0000

    conversion

--- 01_basics/03_conversionorperation.js
+++ 01_basics/03_conversionorperation.js
@@ -1,33 +1,54 @@
-let score = 34 //number
+// let score = 34 //number
 
-let scope = "33" //string
-
-
-console.log(typeof score);
-console.log(typeof(score));
-
-console.log(typeof scope);
-
-// score can directly change into 
-let valueInNumber = Number(scope)
-console.log(typeof valueInNumber);
+// let scope = "33" //string
 
 
-let scopee = null
-let valueNumber = Number(scope)
-console.log(typeof valueNumber);
-console.log(valueNumber)
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log(typeof scope);
+
+// // score can directly change into 
+// let valueInNumber = Number(scope)
+// console.log(typeof valueInNumber);
 
 
-// "33" => 33
-// "33abc"=> NaN
-// true =>1; false=>0
+// let scopee = null
+// let valueNumber = Number(scope)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
 
-let isLoggedIn = null
 
-let boolearnIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolearnIsLoggedIn);
+// // "33" => 33
+// // "33abc"=> NaN
+// // true =>1; false=>0
 
-// 1 => true; 0 => false
-// "" =>false
-// "adi" => true 
+// let isLoggedIn = null
+
+// let boolearnIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolearnIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" =>false
+// // "adi" => true 
+
+
+// ************************ Operations******************
+
+let value = 4;
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2  = "world"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
