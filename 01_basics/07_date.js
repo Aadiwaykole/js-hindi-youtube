1059cc85755f225e73911a3fb12016767ea7443f
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-27 15:52:44 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+console.log(myDate.toString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(23,0,23)
+
+console.log(myCreatedDate.toDateString)
+
+console.log(myCreatedDate.toLocaleString())
+
+console.log(Math.floor(Date.now()/1000));
+
+
+let myyDate = new Date()
+
+console.log(myyDate.getMonth());
