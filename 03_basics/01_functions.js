2f92f8852a30413a1adf31ad88f8fcc8a7b3f423
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-28 19:04:22 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+function addTwoNumber(number1, number2){
+    //let result = number 1 + number2
+    // return result 
+    return number1+number2
+}
+
+// const result =  addTwoNumber(4, 6);
+// console.log("result:", result);
+
