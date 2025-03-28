46d14087f8441da683924c24383f87495890f808
Aadiwaykole <adityawaykole494@gmail.com>
2025-03-28 18:19:34 +0000

    basics objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,19 @@
+const course = {
+    coursename : "js in hindi",
+    price: "0009",
+    courseInstructor: "hitesh"
+
+}
+//couse.courseInstructor //CANT USE
+
+const{courseInstructor:instructor} = course
+console.log(instructor);
+
+// {
+//     // this is json
+// }
+
+{
+    name : "adiya"
+}
+
